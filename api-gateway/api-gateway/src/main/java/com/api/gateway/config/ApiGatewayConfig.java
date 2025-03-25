88a8220a4ad06146b1daa9606b9ec42f35014dbf
avinash <71429749+davinash300@users.noreply.github.com>
2025-03-25 21:56:21 +0530

    Add files via upload

---
+++ api-gateway/api-gateway/src/main/java/com/api/gateway/config/ApiGatewayConfig.java
@@ -0,0 +1,27 @@
+package com.api.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.Collections;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // Frontend URL
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
